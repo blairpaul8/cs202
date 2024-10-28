c823f51311c598fad1067bb68375272e3d2a5066
Paul Blair <blairpaul8@yahoo.com>
2024-10-28 13:18:14 -0400

    Wrote depth and find_height need to test them

--- lab9/src/bstree_lab.cpp
+++ lab9/src/bstree_lab.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <iostream>
 #include <cstdio>
+#include <algorithm>
 #include "bstree.hpp"
 using namespace std;
 using CS140::BSTree;
@@ -10,44 +11,73 @@ using CS140::BSTNode;
 /* ---------------------------------------------- */
 /* You'll write all of these as part of your lab. */
 
-int BSTree::Depth(const string &key) const
-{
-  (void) key;
+int BSTree::Depth(const string &key) const {
+  int depth = 0;
+  BSTNode *n;
+  n = sentinel->right;
+  while (true) {
+    if (key == n->key) {
+      return depth;
+    }
+    if (key < n->key) {
+      depth++;
+      n = n->left;
+    }
+    else {
+      depth++;
+      n = n->right;
+
+    }
+  }
   return -1;
 }
            
-int BSTree::Height() const
-{
+int BSTree::Height() const {
+  BSTNode *n;
+  n = sentinel->right;
+  int height = recursive_find_height(&n);
+  if (height >= 0) {
+    return height;
+  }
   return -1;
 }
 
-vector <string> BSTree::Ordered_Keys() const
-{
+vector <string> BSTree::Ordered_Keys() const {
   vector <string> rv;
   return rv;
 }
     
-BSTree::BSTree(const BSTree &t)        
-{
+BSTree::BSTree(const BSTree &t) {
   (void) t;
 }
 
-BSTree& BSTree::operator= (const BSTree &t) 
-{
+BSTree& BSTree::operator= (const BSTree &t) {
   (void) t;
   return *this;
 }
 
-int BSTree::recursive_find_height(const BSTNode *n) const
-{
-  (void) n;
+int BSTree::recursive_find_height(const BSTNode *n) const {
+  if (n == sentinel) {
+    return 0;
+  }
+
+  h_left = recursive_find_height(n->left) + 1;
+  h_right = recursive_find_height(n->right) + 1;
+
+  if (h_left >= h_right) {
+    return h_left;
+  }
+  else {
+    return h_right;
+  }
   return -1;
 }
 
-void BSTree::make_key_vector(const BSTNode *n, vector<string> &v) const
-{
-  (void) n;
-  (void) v;
+void BSTree::make_key_vector(const BSTNode *n, vector<string> &v) const {
+  if (n == sentinel) return;
+  make_key_vector(n->left, v);
+  v.push_back(n->val);
+  make_key_vector(n->right, v);
 }
 
 BSTNode *BSTree::make_balanced_tree(const vector<string> &sorted_keys, 
